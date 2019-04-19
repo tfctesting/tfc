a0f166f7186ac50dc767a1e8625f31a63e05e338
Markus Ottela <oottela@cs.helsinki.fi>
2019-04-19 08:45:52 +0300

    Update urllib3 and pubkey, remove last assert

--- src/transmitter/contact.py
+++ src/transmitter/contact.py
@@ -198,7 +198,8 @@ def change_nick(user_input:   'UserInput',
     if window.type == WIN_TYPE_GROUP:
         rename_group(nick, window, contact_list, group_list, settings, queues)
 
-    assert window.contact is not None
+    if window.contact is None:
+        raise FunctionReturn("Error: Window does not have contact.")
 
     onion_pub_key = window.contact.onion_pub_key
     error_msg     = validate_nick(nick, (contact_list, group_list, onion_pub_key))
