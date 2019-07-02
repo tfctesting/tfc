d20a2bccebff4c2ca6b22dbc8f01233912ec18bf
Markus Ottela <oottela@cs.helsinki.fi>
2019-07-03 01:02:26 +0300

    Refactor test_crypto.

--- tests/common/test_crypto.py
+++ tests/common/test_crypto.py
@@ -294,9 +294,9 @@ class TestX448(unittest.TestCase):
         force the shared secret to a preselected value. This also
         applies to X448.
             It's not clear how this type of attack could be leveraged in
-        the context of secure messaging where both sender and recipient
-        desire confidentiality, and where easier ways to break the
-        confidentiality of the conversation exist. However, there is
+        the context of secure messaging where both the sender and the
+        recipient desire confidentiality, and where easier ways to break
+        the confidentiality of the conversation exist. However, there is
           a) no harm in doing the check and
           b) no need to trouble ourselves with whether TFC should ensure
              contributory behavior; the pyca/cryptography library
