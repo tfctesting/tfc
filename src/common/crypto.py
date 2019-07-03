3fef79b7bca64ce9072ea3dfafca2ab954150d50
Markus Ottela <oottela@cs.helsinki.fi>
2019-07-03 23:20:17 +0300

    Refactor src.common.crypto

--- src/common/crypto.py
+++ src/common/crypto.py
@@ -339,8 +339,9 @@ class X448(object):
         4. Unlike OpenSSL user-space CSPRNG that only seeds from
            /dev/urandom, the OS random engine uses GETRANDOM(0) syscall
            that sources all of its entropy directly from /dev/urandom.
-           It also does not suffer from the fork() weakness, or have
-           issues with CSPRNG initialization.[6]
+           The OS random engine does not suffer from the fork() weakness
+           where forked process is not automatically reseeded. It's also
+           safe from issues with CSPRNG initialization.[6]
 
         5. The fallback option (/dev/urandom) of OS random engine might
            be problematic on pre-3.17 kernels if the CSPRNG has not been
