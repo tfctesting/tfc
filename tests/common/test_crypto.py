c832f3e55b68869300f4d75547ef6448ba1d3f2c
Markus Ottela <oottela@cs.helsinki.fi>
2019-07-17 09:49:13 +0300

    Refactor test_crypto

--- tests/common/test_crypto.py
+++ tests/common/test_crypto.py
@@ -483,7 +483,7 @@ class TestBytePadding(unittest.TestCase):
         https://github.com/pyca/cryptography/blob/master/tests/hazmat/primitives/test_padding.py
     """
 
-    def test_length_of_padded_message_is_divisible_by_padding_length(self):
+    def test_length_of_the_padded_message_is_divisible_by_padding_length(self):
         padded_bytestrings = set()
 
         for message_length in range(4*PADDING_LENGTH):
