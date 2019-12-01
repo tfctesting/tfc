b03d0ee444420d8b5adffdccb777c8b74357bd66
Markus Ottela <oottela@cs.helsinki.fi>
2019-12-01 22:04:32 +0200

    fix installer

--- install.sh
+++ install.sh
@@ -156,33 +156,33 @@ function process_tails_dependencies {
     # Manage Tails dependencies in batch. The command that uses the
     # files is passed to the function as a parameter.
 
-    t_sudo -E $1 "/opt/tfc/${PYSERIAL}"
-    # t_sudo -E $1 "/opt/tfc/${STEM}"
-    t_sudo -E $1 "/opt/tfc/${PYSOCKS}"
+    t_sudo -E "$1 /opt/tfc/${PYSERIAL}"
+    # t_sudo -E "$1 /opt/tfc/${STEM}"
+    t_sudo -E "$1 /opt/tfc/${PYSOCKS}"
 
     # Requests
-    t_sudo -E $1 "/opt/tfc/${URLLIB3}"
-    t_sudo -E $1 "/opt/tfc/${IDNA}"
-    t_sudo -E $1 "/opt/tfc/${CHARDET}"
-    t_sudo -E $1 "/opt/tfc/${CERTIFI}"
-    t_sudo -E $1 "/opt/tfc/${REQUESTS}"
+    t_sudo -E "$1 /opt/tfc/${URLLIB3}"
+    t_sudo -E "$1 /opt/tfc/${IDNA}"
+    t_sudo -E "$1 /opt/tfc/${CHARDET}"
+    t_sudo -E "$1 /opt/tfc/${CERTIFI}"
+    t_sudo -E "$1 /opt/tfc/${REQUESTS}"
 
     # Flask
-    t_sudo -E $1 "/opt/tfc/${WERKZEUG}"
-    t_sudo -E $1 "/opt/tfc/${MARKUPSAFE}"
-    t_sudo -E $1 "/opt/tfc/${JINJA2}"
-    t_sudo -E $1 "/opt/tfc/${ITSDANGEROUS}"
-    t_sudo -E $1 "/opt/tfc/${CLICK}"
-    t_sudo -E $1 "/opt/tfc/${FLASK}"
+    t_sudo -E "$1 /opt/tfc/${WERKZEUG}"
+    t_sudo -E "$1 /opt/tfc/${MARKUPSAFE}"
+    t_sudo -E "$1 /opt/tfc/${JINJA2}"
+    t_sudo -E "$1 /opt/tfc/${ITSDANGEROUS}"
+    t_sudo -E "$1 /opt/tfc/${CLICK}"
+    t_sudo -E "$1 /opt/tfc/${FLASK}"
 
     # Cryptography
-    t_sudo -E $1 "/opt/tfc/${SIX}"
-    t_sudo -E $1 "/opt/tfc/${PYCPARSER}"
-    t_sudo -E $1 "/opt/tfc/${CFFI}"
-    t_sudo -E $1 "/opt/tfc/${CRYPTOGRAPHY}"
+    t_sudo -E "$1 /opt/tfc/${SIX}"
+    t_sudo -E "$1 /opt/tfc/${PYCPARSER}"
+    t_sudo -E "$1 /opt/tfc/${CFFI}"
+    t_sudo -E "$1 /opt/tfc/${CRYPTOGRAPHY}"
 
     # PyNaCl
-    t_sudo -E $1 "/opt/tfc/${PYNACL}"
+    t_sudo -E "$1 /opt/tfc/${PYNACL}"
 }
 
 
