a2c875b7457c15697b853c51f349ec473164b985
Markus Ottela <oottela@cs.helsinki.fi>
2019-12-02 08:47:54 +0200

    Change statement to comment

--- src/common/reed_solomon.py
+++ src/common/reed_solomon.py
@@ -196,15 +196,14 @@ class ReedSolomonError(Exception):
     pass
 
 
-"""
-For efficiency, gf_exp[] has size 2*GF_SIZE, so that a simple
-multiplication of two numbers can be resolved without calling % 255.
-For more info on how to generate this extended exponentiation table,
-see paper:
-    "Fast software implementation of finite field operations",
-    Cheng Huang and Lihao Xu
-    Washington University in St. Louis, Tech. Rep (2003).
-"""
+# For efficiency, gf_exp[] has size 2*GF_SIZE, so that a simple
+# multiplication of two numbers can be resolved without calling % 255.
+# For more info on how to generate this extended exponentiation table,
+# see paper:
+#     "Fast software implementation of finite field operations",
+#     Cheng Huang and Lihao Xu
+#     Washington University in St. Louis, Tech. Rep (2003).
+
 _bytearray   = bytearray  # type: Any
 gf_exp       = _bytearray([1] * 512)
 gf_log       = _bytearray(256)
