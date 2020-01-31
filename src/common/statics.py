26d935b9ff53ad5a693a8158d424ac55c868c488
Markus Ottela <oottela@cs.helsinki.fi>
2020-01-31 22:36:55 +0200

    Bump version number and update Jinja2

--- src/common/statics.py
+++ src/common/statics.py
@@ -21,7 +21,7 @@ along with TFC. If not, see <https://www.gnu.org/licenses/>.
 
 """Program details"""
 TFC         = 'TFC'
-VERSION     = '1.20.01'
+VERSION     = '1.20.02'
 TRANSMITTER = 'Transmitter'
 RECEIVER    = 'Receiver'
 RELAY       = 'Relay'
