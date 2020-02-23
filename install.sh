93df1bb94d621bc592fcb65b2f733cc97deba27e
Markus Ottela <oottela@cs.helsinki.fi>
2020-02-23 02:24:15 +0200

    Initial Qubes TCB install configuration

--- install.sh
+++ install.sh
@@ -312,6 +312,47 @@ function steps_before_network_kill {
 }
 
 
+function install_qubes_tcb {
+    # Qubes installation configuration for TCB domains.
+
+    sudo apt update
+    sudo apt install libssl-dev python3-pip python3-tk --yes
+
+    sudo git clone --depth 1 https://github.com/tfctesting/tfc.git /opt/tfc
+
+    sudo python3.7 -m pip download --no-cache-dir -r "/opt/tfc/requirements-venv.txt" --require-hashes --no-deps -d /opt/tfc/
+    sudo python3.7 -m pip download --no-cache-dir -r "/opt/tfc/requirements.txt"      --require-hashes --no-deps -d /opt/tfc/
+
+    kill_network
+
+    verify_files
+
+    create_user_data_dir
+
+    sudo python3.7 -m pip install "/opt/tfc/${VIRTUALENV}"
+    sudo python3.7 -m virtualenv  "/opt/tfc/venv_tcb" --system-site-packages --never-download
+
+    . /opt/tfc/venv_tcb/bin/activate
+    process_tcb_dependencies "python3.7 -m pip install"
+    deactivate
+
+    sudo mv /opt/tfc/tfc.png                   /usr/share/pixmaps/
+    sudo mv /opt/tfc/launchers/TFC-TxP.desktop /usr/share/applications/
+    sudo mv /opt/tfc/launchers/TFC-RxP.desktop /usr/share/applications/
+
+    # Remove unnecessary files
+    remove_common_files      "sudo"
+    process_tcb_dependencies "rm"
+    sudo rm -r /opt/tfc/src/relay/
+    sudo rm    /opt/tfc/dd.py
+    sudo rm    /opt/tfc/relay.py
+    sudo rm    /opt/tfc/tfc.yml
+    sudo rm    /opt/tfc/${VIRTUALENV}
+
+    install_complete "Installation of TFC on this device is now complete."
+}
+
+
 function install_tcb {
     # Install TFC for Source/Destination Computer.
     #
@@ -766,6 +807,7 @@ sudo_pwd=''
 
 case $1 in
     tcb   ) install_tcb;;
+    qtcb  ) install_qubes_tcb;;
     relay ) install_relay;;
     local ) install_local_test;;
     dev   ) install_developer;;
