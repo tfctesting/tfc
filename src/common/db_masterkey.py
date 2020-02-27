a031d376d18c7c94373fd6c16637f49d60915834
Markus Ottela <oottela@cs.helsinki.fi>
2020-02-28 00:47:06 +0200

    typo

--- src/common/db_masterkey.py
+++ src/common/db_masterkey.py
@@ -263,7 +263,7 @@ class MasterKey(object):
 
             # Sentinel that checks if the binary search has ended,
             # and that restarts the search if kd_time repeats. This
-            # prevents and Alderson loop.
+            # prevents an Alderson loop.
             if upper_bound is not None and kd_time in [lower_bound, upper_bound]:
                 lower_bound = ARGON2_MIN_TIME_COST
                 upper_bound = None
