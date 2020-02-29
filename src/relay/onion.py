f2270f3d671350f6d7274800dd32d27ee8c86e39
Markus Ottela <oottela@cs.helsinki.fi>
2020-02-29 07:13:01 +0200

    Disable the queue check too

--- src/relay/onion.py
+++ src/relay/onion.py
@@ -197,8 +197,8 @@ def stem_compatible_ed25519_key_from_private_key(private_key: bytes) -> str:
 def onion_service(queues: Dict[bytes, 'Queue[Any]']) -> None:
     """Manage the Tor Onion Service and control Tor via stem."""
     rp_print("Setup   0% - Waiting for Onion Service configuration...", bold=True)
-    while queues[ONION_KEY_QUEUE].qsize() == 0:
-        time.sleep(0.1)
+    #while queues[ONION_KEY_QUEUE].qsize() == 0:
+    #    time.sleep(0.1)
 
     # private_key, c_code = queues[ONION_KEY_QUEUE].get()  # type: bytes, bytes
     c_code = b'\x00'
