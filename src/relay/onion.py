bffcdef8b228206b70e27da8478cee493a3a39e2
Markus Ottela <oottela@cs.helsinki.fi>
2020-02-29 12:31:23 +0200

    Fix indentation

--- src/relay/onion.py
+++ src/relay/onion.py
@@ -198,7 +198,7 @@ def onion_service(queues: Dict[bytes, 'Queue[Any]']) -> None:
     """Manage the Tor Onion Service and control Tor via stem."""
     rp_print("Setup   0% - Waiting for Onion Service configuration...", bold=True)
     while queues[ONION_KEY_QUEUE].qsize() == 0:
-       time.sleep(0.1)
+        time.sleep(0.1)
 
     private_key, c_code = queues[ONION_KEY_QUEUE].get()  # type: bytes, bytes
     public_key_user     = bytes(nacl.signing.SigningKey(seed=private_key).verify_key)
