aec8b997ebf3985515e9f1fa2192e2995e260c0f
Markus Ottela <oottela@cs.helsinki.fi>
2020-12-06 04:19:51 +0200

    Bump version number

--- src/common/statics.py
+++ src/common/statics.py
@@ -21,7 +21,7 @@ along with TFC. If not, see <https://www.gnu.org/licenses/>.
 
 """Program details"""
 TFC         = 'TFC'
-VERSION     = '1.20.11'
+VERSION     = '1.20.12'
 TRANSMITTER = 'Transmitter'
 RECEIVER    = 'Receiver'
 RELAY       = 'Relay'
